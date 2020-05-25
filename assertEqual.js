12dcca5566a6ad01641bdb6b8f89d0c5555b4841
Andrew Delmar <andrewjdelmar@gmail.com>
2020-05-25 23:54:14 +0000

    assertEqual assignment

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  let output = "";
+
+  if (actual === expected) {
+    output += ("🛑🛑🛑 " + "Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    output += ("✅✅✅ " + "Assertion Failed " + actual + " !== " + expected);
+  }
+
+  return output;
+};
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
