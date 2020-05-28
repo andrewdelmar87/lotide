98109c370dfae2f835095338e35747b009bf82b1
Andrew Delmar <andrewjdelmar@gmail.com>
2020-05-28 22:43:02 +0000

    Updates

--- eqArrays.js
+++ eqArrays.js
@@ -11,18 +11,15 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  
   if (arr1.length !== arr2.length) {
     return false;
   }
-
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
-  
 };
 
 console.log(assertEqual(eqArrays([1, 4, 3], [1, 2, 3]), true));
