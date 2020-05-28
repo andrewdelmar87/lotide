6b257e2df7473ce813222567aa2f78e92e108480
Andrew Delmar <andrewjdelmar@gmail.com>
2020-05-28 03:22:37 +0000

    Original add

---
+++ eqObjects.js
@@ -0,0 +1,38 @@
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (let key in object1) {
+      if (object1[key].constructor === Array) {
+        if (object1[key].length !== object2[key].length) {
+          return false;
+        }
+        for (let i = 0; i === object1[key].length - 1; i++) {
+          if (object1[key][i] !== object2[key][i]) {
+            return false;
+          }
+        }
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  let output = "";
+
+  if (actual === expected) {
+    output += (`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    output += (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+  return output;
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+
+console.log(assertEqual(eqObjects(cd, dc), false));
